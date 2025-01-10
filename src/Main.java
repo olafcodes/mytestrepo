7422b9eff8f5f16d09da9541705e42cc7a9495bc
olafcodes <olafl@nycstudents.net>
2025-01-10 10:44:34 -0500

    tenzin and showers in the same sentence?

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("lol");
         System.out.println("sigma boy");
+        System.out.println("Tenzin reeks");
     }
 
 }
